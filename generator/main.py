4b76545e97c5465c2d17836f400e4b4fc949dc48
VinceSato <mensatovicenzo@gmail.com>
2023-11-14 15:24:28 -0300

    Adding comments

--- generator/main.py
+++ generator/main.py
@@ -1,5 +1,7 @@
 from password_generator import password_generator
 
 
+# Run the program. Enjoy!
+
 if __name__ == "__main__":
     password_generator()
--- generator/password_generator.py
+++ generator/password_generator.py
@@ -2,7 +2,12 @@ from random import sample
 
 def password_generator():
 
+    # User input: desired password length
+
     password_length = int(input("Thank you for using our password generator!\n Please pick the desired password length:\n"))
+
+    # Characters' variables
+
     password_lower = "abcdefghijklmnopqrstuvwxyz"
     password_upper = password_lower.upper()
     numbers = "0123456789"
@@ -11,9 +16,16 @@ def password_generator():
     print(f"Understood. Your password has a length of {password_length} digits!")
     print('Our standard password generator contains lower and uppercase letters, as well as numbers.\nWould you like to include special characters? Type "yes" or "y" to accept.')
 
+    # After asking if the user wants to include special characters, store it in a variable
+
     has_special = input()
 
+    # The standard password will contatin, lowercase/uppercase letters and numbers
+
     password_chars = password_lower + password_upper + numbers
+
+    # This code snippet includes special characters in the generated password in case the user says "yes" to the question
+
     if has_special.lower() in ("yes", "y"):
         password_chars += password_special
         print("Alright. Special characters enabled.")
